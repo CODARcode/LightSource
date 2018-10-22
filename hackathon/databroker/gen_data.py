39bbc217665a69f12d05292fb8adcf8db4533239
Maksim Rakitin <mrakitin@bnl.gov>
2018-10-22 17:04:12 +0000

    Add BestEffortCallback to RE

--- hackathon/databroker/gen_data.py
+++ hackathon/databroker/gen_data.py
@@ -2,11 +2,14 @@ import databroker
 import bluesky.plans as bp
 from bluesky import RunEngine
 from bluesky.callbacks.zmq import Publisher
+from bluesky.callbacks.best_effort import BestEffortCallback
 from ophyd.sim import motor, det
 
 db = databroker.Broker.named('local')
+bec = BestEffortCallback()
 RE = RunEngine()
 RE.subscribe(db.insert)
+RE.subscribe(bec)
 
 pub = Publisher('localhost:2000', RE=RE)
 
