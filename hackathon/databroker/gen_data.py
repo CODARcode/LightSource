b476c08da2d8d483dc069eabe1bf4da3bc31d8c8
Maksim Rakitin <mrakitin@bnl.gov>
2018-10-22 08:10:52 +0000

    DEV: add 0MQ communication between RE and swift-t

--- hackathon/databroker/gen_data.py
+++ hackathon/databroker/gen_data.py
@@ -1,11 +1,14 @@
-from bluesky import RunEngine
 import databroker
 import bluesky.plans as bp
+from bluesky import RunEngine
+from bluesky.callbacks.zmq import Publisher
 from ophyd.sim import motor, det
 
 db = databroker.Broker.named('local')
 RE = RunEngine()
 RE.subscribe(db.insert)
+
+pub = Publisher('localhost:2000', RE=RE)
 
 for j in range(10):
     RE(bp.scan([det], motor, -10, 10, 100), sample=f'sample_{j}')
